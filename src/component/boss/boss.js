35dd034137e592c37bb4deb39201b30c9b8d5e8e
WY <you@example.com>
2019-01-16 11:14:05 +0800

    用redux实现列表渲染

--- src/component/boss/boss.js
+++ src/component/boss/boss.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import axios from 'axios'
+import { connect } from 'react-redux'
 import { Card, WingBlank, WhiteSpace} from 'antd-mobile'
+import { getUserList } from '../../redux/chatuser.redux'
 
+@connect(
+    state=>state.chatuser,
+    {
+        getUserList
+    }
+)
 class Boss extends React.Component{
     constructor(props){
         super(props)
@@ -11,21 +18,16 @@ class Boss extends React.Component{
     }
 
     componentDidMount(){
-        axios.get('/user/list?type=genius').then(res => {
-            if(res.data.code == 200){
-                this.setState({
-                    data: res.data.body
-                })
-            }
-        })
+        this.props.getUserList('genius')
     }
+
 
     render(){
         console.log(this.state.data)
         return (
             <div>
                 <WingBlank>
-                    {this.state.data.map(v=>(
+                    {this.props.userlist.map(v=>(
                         v.avatar?
                         <Card key={v._id}>
                             <Card.Header
